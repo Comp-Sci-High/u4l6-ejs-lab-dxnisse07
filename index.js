4f3dc9be91ff0a2f3685afaa0a871ee7c7ecf853
dxnisse07 <denisse.gonzalez25@compscihigh.org>
2025-01-24 15:31:24 +0000

    porgress fro day 1 , almost done

--- index.js
+++ index.js
@@ -74,26 +74,30 @@ app.use((req, res, next) => {
 
 // Task 1: Set up the static middleware
 
-
+app.use(express.static(__dirname + "/public"))
 
 // Task 2: Set up the route handler for / to send back the index.html file
-
-
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname + '/public/index.html')
+})
 
 // Task 3: Set up the route handler for /mens which sends back category.ejs with the men's array
-
-
+app.get("/mens",(req,res)=>{
+  const data= inventory[0];
+   res.render("category.ejs", inventory[0])
+});
 
 // Task 4: Plug in the values in category.ejs to get the page working
-// PINK ONLY: Set up a route handler for /womens to pass in similar data for women's
-
-
 
 // Task 5: Set up the route handler for /item/0 which sends back the first item in product.ejs
-
+app.get("/item/0",(req,res)=>{
+  const data= inventory[0]
+  res.render("product.ejs", data)
+})
 
 
 // Task 6: Plug in the values in product.ejs to get the page working
+
 // Extra credit: modify the /item/0 route handler to have dynamic path parameter and return any item's data
 
 app.listen(3000, () => {
